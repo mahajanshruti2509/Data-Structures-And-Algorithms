3006afdc3db7349bc70c1444486b6b321c1e4921
Shruti Mahajan <shrutimahajan@shrutis-mbp.lan>
2020-07-22 21:59:26 -0500

    Binary Tree Zig Zag Level Order Traversal

---
+++ Tree/103_BinaryTreeZigZagLevelOrder.java
@@ -0,0 +1,66 @@
+/***********************************************
+Time Complexity : O(n)
+Space Complexity : O(n)
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+***********************************************/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        
+        s1.push(root);
+        while(!s1.isEmpty() || !s2.isEmpty()) {
+            
+            List<Integer> currentLevel1 = new ArrayList<>();
+            while(!s1.isEmpty()) {
+                TreeNode current = s1.pop();
+                currentLevel1.add(current.val);
+                if(current.left != null) {
+                    s2.add(current.left);
+                }
+                if(current.right != null) {
+                    s2.add(current.right);
+                }
+            }
+            if(currentLevel1.size() > 0) {
+                result.add(currentLevel1);
+            }
+            
+            List<Integer> currentLevel2 = new ArrayList<>();
+            while(!s2.isEmpty()) {
+                TreeNode current = s2.pop();
+                currentLevel2.add(current.val);
+                if(current.right != null) {
+                    s1.add(current.right);
+                }
+                if(current.left != null) {
+                    s1.add(current.left);
+                }
+            }
+            if(currentLevel2.size() > 0) {
+                result.add(currentLevel2);
+            }
+        }// outer while 
+        return result;
+    }
+}
