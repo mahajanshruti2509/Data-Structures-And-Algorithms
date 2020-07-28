ab31cfcce8220f3c13e5495fd19e316217fbf0aa
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-07-27 22:47:05 -0500

    Binary Tree from Inorder and Post Order

---
+++ Tree/106_BinaryTreeFromInorder&PostorderTraversal.java
@@ -0,0 +1,56 @@
+/***********************************************
+Time Complexity : O(n)
+Space Complexity : O(n)
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/discuss/34782/My-recursive-Java-code-with-O(n)-time-and-O(n)-space
+***********************************************/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int pInorder;   // index of inorder array
+    int pPostorder; // index of postorder array
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        pInorder = inorder.length - 1;
+        pPostorder = postorder.length - 1;
+
+        return buildTree(inorder, postorder, null);
+    }
+
+    private TreeNode buildTree(int[] inorder, int[] postorder, TreeNode end) {
+        if (pPostorder < 0) {
+            return null;
+        }
+
+        // create root node
+        TreeNode n = new TreeNode(postorder[pPostorder--]);
+
+        // if right node exist, create right subtree
+        if (inorder[pInorder] != n.val) {
+            n.right = buildTree(inorder, postorder, n);
+        }
+
+        pInorder--;
+
+        // if left node exist, create left subtree
+        if ((end == null) || (inorder[pInorder] != end.val)) {
+            n.left = buildTree(inorder, postorder, end);
+        }
+
+        return n;
+    }
+}
