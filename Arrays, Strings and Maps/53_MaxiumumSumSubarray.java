69e562da024d29bffd69c7528623709ee781461d
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-08-10 22:38:53 -0500

    Maximum sum subarray

---
+++ Arrays, Strings and Maps/53_MaxiumumSumSubarray.java
@@ -0,0 +1,18 @@
+/***********************************************
+Time Complexity : O(n)
+Space Complexity : O(1)
+https://leetcode.com/problems/maximum-subarray/
+***********************************************/
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = maxSum;
+        
+        for(int i = 1 ; i < nums.length; i++) {
+            currentSum = Math.max(currentSum + nums[i], nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+}
