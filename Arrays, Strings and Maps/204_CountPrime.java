1808cd6c761e6714e5e8c230160777de14070e66
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-08-16 23:32:20 -0500

    Count Primes

---
+++ Arrays, Strings and Maps/204_CountPrime.java
@@ -0,0 +1,34 @@
+/***********************************************
+Time Complexity : O(n log(logn))
+Space Complexity : O(n)
+https://leetcode.com/problems/count-primes/
+https://www.youtube.com/watch?v=eKp56OLhoQs
+***********************************************/
+
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 1) {
+            return 0;
+        }
+        
+        boolean[] notPrime = new boolean[n];
+        notPrime[0] = true; 
+        notPrime[1] = true; 
+        
+        for(int i = 2; i < Math.sqrt(n); i++) {
+            if(!notPrime[i]) {
+                for(int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true; 
+                }
+            }
+        }
+        
+        int count = 0; 
+        for(int i = 2; i < notPrime.length; i++) {
+            if(!notPrime[i]){
+                count++;
+            } 
+        }
+        return count; 
+    }
+}
