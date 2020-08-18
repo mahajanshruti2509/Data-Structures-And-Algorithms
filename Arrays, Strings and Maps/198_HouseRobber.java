d83920a15e9729204ccfe396982e476b3369aede
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-08-17 23:27:49 -0500

    Rotate Array

--- Arrays, Strings and Maps/198_HouseRobber.java
+++ Arrays, Strings and Maps/198_HouseRobber.java
@@ -4,9 +4,6 @@ Space Complexity : O(n)
 https://leetcode.com/problems/house-robber/
 ***********************************************/
 
-
-
-
 /* A robber has 2 options: a) rob current house i; b) don't rob current house.
 If an option "a" is selected it means she can't rob previous i-1 house but can safely proceed to the one before previous i-2 and gets all cumulative loot that follows.
 If an option "b" is selected the robber gets all the possible loot from robbery of i-1 and all the following buildings.
