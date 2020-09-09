9d451526609d144c3d40e2283f0fb8bac9840b6a
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-09-08 22:29:19 -0500

    Longest Increasing Subsequence

---
+++ Dynamic Programming/300_LongestIncreasingSubsequence.java
@@ -0,0 +1,28 @@
+/***********************************************
+Time Complexity : O(n^2)
+Space Complexity : O(n)
+https://leetcode.com/problems/longest-increasing-subsequence/
+https://www.youtube.com/watch?v=mouCn3CFpgg
+***********************************************/
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int result = 1;
+        
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }  
+            }
+            result = Math.max(result, dp[i]); 
+        }
+        return result;
+    }
+}
