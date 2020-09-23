7651d3da8737efa7b2d2becf204e056d86f2cb96
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-09-22 23:40:04 -0500

    Decode ways

--- Dynamic Programming/91_DecodeWays.java
+++ Dynamic Programming/91_DecodeWays.java
@@ -13,7 +13,7 @@ class Solution {
         
         int dp[] = new int[s.length() + 1];
         dp[0] = 1;
-        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1; // single digit 0 is invalid | 01, 02 is invalid | 10, 20 is valid;
         
         for(int i = 2; i <= s.length(); i++) {
             int oneDigit = Integer.valueOf(s.substring(i-1, i));
