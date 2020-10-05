f426f08b33e62970d55632ed5958b5ff8fcfccb8
Shruti Mahajan <mahajanshruti.2509@gmail.com>
2020-10-04 22:54:37 -0500

    Container with most water

---
+++ Arrays, Strings and Maps/11_ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+/***********************************************
+Time Complexity : O(n)
+Space Complexity : O(1)
+https://leetcode.com/problems/container-with-most-water/
+***********************************************/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = Integer.MIN_VALUE;
+        int left = 0;
+        int right = height.length - 1;
+        while(left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int width = right - left;
+            int currentArea =  minHeight * width;
+            maxArea = Math.max(maxArea, currentArea);
+            if(height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
